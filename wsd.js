df8d3a5fb0354f7d1ba6c9112b7003e77ce5fdaf
takasmiley <takasmiley@live.jp>
2019-02-02 17:37:05 +0900

    add start and build command

--- wsd.js
+++ wsd.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const start = require('./lib/start');
+const build = require('./lib/build');
 // const fs = require('fs-extra')
 // const path = require('path');
 // const globby = require('globby');
@@ -10,19 +12,27 @@ program
   .version('1.0.0');
 
 program
-  .command('build <site-config>')
-  .description('build site')
-  .option('-d, --destination <value>', 'Destination directory')
-  .action(function(siteConfig, options) {
-    console.log('build', siteConfig, options);
-  });
-
-program
   .command('start <site-config>')
   .description('start site')
   .option('-D, --debug', 'Debug mode')
   .action(function(siteConfig, options) {
-    console.log('start', siteConfig, options);
+    start(siteConfig).catch((err) => {
+      if (err) {
+        console.error(err);
+      }
+      process.exit(1);
+    });
   });
-
+program
+  .command('build <site-config>')
+  .description('build site')
+  .option('-d, --destination <value>', 'Destination directory')
+  .action(function(siteConfig, options) {
+    build(siteConfig).catch((err) => {
+      if (err) {
+        console.error(err);
+      }
+      process.exit(1);
+    });
+  });
 program.parse(process.argv);
