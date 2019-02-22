afa89881550b04a1e4c465f44f3c3f18a16c1ea8
takasmiley <takasmiley@live.jp>
2019-02-23 02:09:26 +0900

    build

--- app/meta-mixin-server.js
+++ app/meta-mixin-server.js
@@ -3,7 +3,9 @@ import { getMeta } from './meta-mixin';
 export default {
   created() {
     const meta = getMeta(this);
-    console.log(`created: this.$root.$options.ssrContext=${this.$root.$options.ssrContext}`);
-    this.$root.$options.ssrContext.meta = meta;
+
+    if (this.$ssrContext) {
+      this.$ssrContext.meta = meta;
+    }
   }
 };
