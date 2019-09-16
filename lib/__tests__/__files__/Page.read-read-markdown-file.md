a119bb0858c676e163a3ac977b71fb703864e086
takasmiley <takasmiley@live.jp>
2019-09-16 20:44:00 +0900

    test Page.read

---
+++ lib/__tests__/__files__/Page.read-read-markdown-file.md
@@ -0,0 +1,8 @@
+---
+title: Markdown title
+description: Markdown description
+---
+# Markdown page
+
+line1
+line2
---
+++ lib/__tests__/__files__/Page.read-read-vue-file.vue
@@ -0,0 +1,22 @@
+<head>
+{
+  "title": "VuePage title",
+  "description": "VuePage description"
+}
+</head>
+
+<template>
+  <div>
+    <h1>Vue Page</h1>
+  </div>
+</template>
+
+<script>
+module.exports = {};
+</script>
+
+<style module>
+h1 {
+  color: red;
+}
+</style>
