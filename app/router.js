f4f5ae637b0159c7cf663a2c1506cb4dbb9ceec3
takasmiley <takasmiley@live.jp>
2019-10-22 23:31:36 +0900

    fmt

--- app/router.js
+++ app/router.js
@@ -8,16 +8,16 @@ export function createRouter() {
   return new Router({
     mode: 'history',
     routes,
-    scrollBehavior (to, from, savedPosition) {
+    scrollBehavior(to, from, savedPosition) {
       if (savedPosition) {
         return savedPosition;
       } else if (to.hash) {
         return {
-          selector: to.hash
+          selector: to.hash,
         };
       } else {
         return { x: 0, y: 0 };
       }
-    }
+    },
   });
 }
